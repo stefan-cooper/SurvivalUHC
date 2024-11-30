e0729df3e2f08945726cfed162bfec30b3eb89f8
Stefan Cooper <stefan.cooper27@gmail.com>
2024-11-30 21:38:20 +0000

    feat: add more details to publish message (#88)
    * chore: bump version
    
    * feat: add more details to publish message

--- publish.sh
+++ publish.sh
@@ -11,11 +11,13 @@ fi
 
 PLUGIN_VERSION=$(mvn help:evaluate -Dexpression=UHCPlugin.version -q -DforceStdout)
 PLUGIN_LOCATION="./build/SpigotUHC-${PLUGIN_VERSION}.jar"
+COMMIT_MSG=${COMMIT_MSG:-$(git log -1 --pretty=format:"%s")}
+COMMIT_SHA=${COMMIT_SHA:-$(git rev-parse HEAD)}
 
 curl --fail-with-body \
   -H "Content-Type: multipart/form-data" \
   -X POST \
-  -F "payload_json={ \"content\":\"SpigotUHC ${PLUGIN_VERSION} is now available\" }" \
+  -F "payload_json={ \"content\":\"**SpigotUHC ${PLUGIN_VERSION}** is now available!\nCommit: ${COMMIT_MSG}\nView changes: <https://github.com/stefan-cooper/SpigotUHC/commit/${COMMIT_SHA}> \" }" \
   -F "file1=@${PLUGIN_LOCATION}" \
   "${DISCORD_WEBHOOK}"
 
